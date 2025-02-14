57492119721e2bacb4eb27425347917ad336398b
Amitai Sopher <amitaisopher@gmail.com>
2025-02-14 12:57:22 +0200

    enhance SlotMachine component with credit initialization and random symbol generation

--- frontend/src/components/SlotMachine.jsx
+++ frontend/src/components/SlotMachine.jsx
@@ -1,27 +1,60 @@
 import { useState } from "react";
 import SlotMachineSymbol from "./SlotMachineSymbol";
 import CustomButton from "./PlayButton";
-import { getValueFromLocalStorage, storeValueToLocalStorage } from "../utils/helpers";
+import { 
+    getValueFromLocalStorage, 
+    storeValueToLocalStorage,
+    getRandomElementFromArray
+ } from "../utils/helpers";
 
-const SlotMachine = () => {
-    const [credit, SetCredit] = useState(getValueFromLocalStorage("credit", 10));
+function initalizeCredit() {
+    const initialCredit =  getValueFromLocalStorage("credit", 10);
+    if (initialCredit <= 0) {
+        storeValueToLocalStorage("credit", 10)
+        return 10;
+    }
+    return initialCredit;
+}
+
+const SlotMachine = ({numberOfSymbols = 3, symbolsSet=["🍒", "🍉", "🍋", "🍍"]}) => {
+    const [credit, SetCredit] = useState(initalizeCredit);
+    const [symbols, setSymbols] = useState(Array.from({ length: numberOfSymbols }, () => symbolsSet[0]));
     const noMoreCredit = credit <= 0;
+    const playButtonClickHandler = async () => {
+        console.log("Play button component was clicked")
+        setSymbols(symbols.map(() => "X"))
+            setTimeout(() => {
+                const newSymbols  = symbols.map(() => getRandomElementFromArray(symbolsSet));
+                setTimeout(() => {
+                    // replace only the first symbol with the new one
+                    setSymbols((prev) => [newSymbols[0], prev[1], prev[2]]);
+                }, 0);
+                setTimeout(() => {
+                    // replace only the second symbol with the new one
+                    setSymbols((prev) => [prev[0], newSymbols[1], prev[2]]);
+                }, 1000);
+                setTimeout(() => {
+                    // replace only the third symbol with the new one
+                    setSymbols((prev) => [prev[0], prev[1], newSymbols[2]]);
+                }, 2000);
+            }, 1000);        
+        
+        storeValueToLocalStorage("credit", credit - 1)
+        SetCredit((prev) => prev - 1 )
+    };
     
 return (
     <div className="slot-machine">
       <div className="slot-machine-symbols-row">
-        <SlotMachineSymbol />
-        <SlotMachineSymbol />
-        <SlotMachineSymbol />
+        {symbols.map((symbol, index) => (
+            <SlotMachineSymbol key={index} symbol={symbol} />
+        ))}
+
       </div>
       <CustomButton
         className={`play-button ${noMoreCredit ? "disabled" : ""}`}
         title="PLAY"
-        onClickHandler={() => {
-            console.log("Play button component was clicked")
-            storeValueToLocalStorage("credit", credit - 1)
-            SetCredit((prev) => prev - 1 )
-        }}
+        onClickHandler={playButtonClickHandler}
         disabled={noMoreCredit}
       />
       <CustomButton
--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -5,3 +5,11 @@ export function storeValueToLocalStorage(key, value) {
 export function getValueFromLocalStorage(key, defaultValue = null) {
   return JSON.parse(localStorage.getItem(key)) ?? defaultValue;
 }
+
+export function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export function getRandomElementFromArray(array) {
+  return array[getRandomInt(0, array.length - 1)];
+}
